42378b6c0f7480f5a780a42faff0f798170d9822
Julianpse <julianpse@gmail.com>
2018-08-16 21:39:03 -0500

    file structure

--- corrosionApp/urls.py
+++ corrosionApp/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from main_app import views
 
 urlpatterns = [
+    url(r'^$', views.index, name='index'),
+    url(r'^main_app/', include('main_app.urls')),
     url(r'^admin/', admin.site.urls),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    my_dict = {'helloWorld': 'helloWorld'}
+    return render(request,'main_app/index.html',context = my_dict)
