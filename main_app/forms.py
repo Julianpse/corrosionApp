f3f2a85d412fa3c81a9c2e6e16a4b51cf2ddfa29
Julianpse <julianpse@gmail.com>
2018-08-18 13:36:08 -0500

    added views

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
-class HomeForm(forms.Form):
-    facility = forms.CharField()
-    equipment_name = forms.CharField()
-    TML = forms.CharField()
+class TechForm(forms.Form):
+    facility = forms.ChoiceField()
+    equipment_name = forms.ChoiceField()
+    TML = forms.ChoiceField()
+    Reading = forms.FloatField(max_value=1)
+
+class CreateEquipmentForm(forms.Form):
+    equipment_name = forms.ChoiceField()
+
+class ViewEquipmentData(forms.Form):
+    equipment_name = forms.ChoiceField()
