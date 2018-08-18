6bac7669815ecf7e8d5654ba4c7826cbf947402d
Julianpse <julianpse@gmail.com>
2018-08-18 11:45:59 -0500

    stuff

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    my_dict = {'helloWorld': 'helloWorld'}
+def home(request):
     return render(request,'main_app/index.html',context = my_dict)
