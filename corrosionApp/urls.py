9ae87d6fd8e8fc972b3b71bc65171a52cf75742b
Julianpse <julianpse@gmail.com>
2018-08-18 14:09:48 -0500

    front end styling

--- corrosionApp/urls.py
+++ corrosionApp/urls.py
@@ -19,6 +19,7 @@ from django.conf.urls import include
 from main_app import views
 
 urlpatterns = [
+    url(r'^$', views.home, name = 'home'),
     url(r'^tech/$', views.tech, name='tech'),
     url(r'^create_equipment/$', views.create_equipment, name='create_equipment'),
     url(r'^view_data/$', views.view_data, name='view_data'),
